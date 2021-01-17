33bc758236003f2e70e0425f80cb95078021274c
Grant Oladipo <grant@oladipo.us>
2021-01-17 18:24:15 -0500

    Make under construction view model public and available on oladipokit

---
+++ Sources/OladipoKit/ui/models/UnderConstructionUIModel.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Grant Oladipo on 1/17/21.
+//
+
+import Foundation
+
+public struct UnderConstructionUIModel {
+    public let constructionMessage: String
+}
